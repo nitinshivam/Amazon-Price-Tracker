229f4a717cd34f17dd9c9e35927611eefd5cb30b
Nitin Shivam <nitinshivam2000@gmail.com>
2020-10-18 14:23:04 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,111 @@
+const axios = require('axios').default;
+const mongoose = require('mongoose');
+const cheerio = require('cheerio');
+const TelegramBot = require('node-telegram-bot-api');
+const dotenv = require('dotenv').config();
+const token = process.env.BOT_TOKEN;
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+});
+
+const userSchema = new mongoose.Schema({
+    userId: {
+        type: Number,
+        trim: true,
+        unique: true
+    },
+    urls: {
+        type: Array
+    }
+});
+
+const user = mongoose.model('url', userSchema, 'users');
+
+
+
+const bot = new TelegramBot(token, { polling: true });
+
+
+async function getPrice(url) {
+    const res = await axios.get(url, {
+        headers: {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.75 Safari/537.36"
+        }
+    });
+
+    const $ = cheerio.load(res.data);
+    const price = $("#priceblock_ourprice").text().replace(/[^0-9.]+/g, '');
+    return price;
+}
+
+bot.onText(/\/start/, (msg) => {
+    const chatId = msg.chat.id;
+
+    user.findOne({ userId: chatId })
+        .then((data) => {
+            if (data === null) {
+                const insert = new user({ userId: chatId });
+                insert.save().then(() => {
+                    console.log(insert);
+                });
+            }
+        })
+
+
+})
+
+bot.onText(/\/add (.+)/, async(msg, match) => {
+
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+
+
+    user.findOneAndUpdate({ userId: chatId }, { $push: { urls: resp } }, ((err, data) => {
+        if (err) {
+            console.log(err);
+        } else {
+            bot.sendMessage(chatId, "Link added sucessfully");
+        }
+    }))
+
+
+});
+
+
+bot.onText(/\/track/, async(msg) => {
+    let currentPrice = [];
+    const chatId = msg.chat.id;
+    let userdetail = await user.findOne({ userId: chatId });
+    console.log(userdetail);
+    for (let i = 0; i < userdetail.urls.length; i++) {
+
+        let price = await getPrice(userdetail.urls[i]);
+        currentPrice.push(price);
+        console.log(currentPrice);
+    }
+
+    let autocheck = setInterval(async() => {
+
+        for (let i = 0; i < userdetail.urls.length; i++) {
+
+            let latestPrice = await getPrice(userdetail.urls[i]);
+            if (latestPrice == currentPrice[i]) {
+                console.log(`latest Price ${latestPrice}`);
+                console.log(`current Price ${currentPrice[i]}`);
+                continue;
+            } else {
+                bot.sendMessage(chatId, `Price dropped to ${latestPrice} of ${userdetail.urls[i]}`);
+                clearInterval(autocheck);
+            }
+        }
+
+    }, 10000);
+
+})
+
+
+// getData(url);
---
+++ test.js
@@ -0,0 +1,8 @@
+for (let i = 0; i < 3; i++) {
+    if (true) {
+        console.log("this is if block");
+        continue;
+    } else {
+        console.log("this is else block");
+    }
+}
